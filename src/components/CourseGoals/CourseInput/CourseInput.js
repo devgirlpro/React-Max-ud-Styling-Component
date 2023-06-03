796a853a87a35a0af48d5eeefb44c476179ae170
Azadeh Galidari <agalidari@gmail.com>
2023-06-03 14:49:27 +0200

    changed style conditionaly with css class

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,31 +5,31 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
-    const [isValid, setIsValid] = useState(true)
+    const [isValid, setIsValid] = useState(true);
 
     const goalInputChangeHandler = (event) => {
         setEnteredValue(event.target.value);
         if (enteredValue.trim().length > 0) {
-          setIsValid(true)
+            setIsValid(true);
         }
     };
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
         if (enteredValue.trim().length === 0) {
-          setIsValid(false)
+            setIsValid(false);
             return;
         }
-        
+
         props.onAddGoal(enteredValue);
         setEnteredValue("");
     };
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <label style={{ color: !isValid  ? 'red' : 'black' }}>Course Goal</label>
-                <input style={{ borderColor: !isValid  ? 'red' : 'black', background: !isValid  ? 'salmon' : 'transparent' }} type="text" onChange={goalInputChangeHandler} />
+            <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler} />
             </div>
             <Button type="submit">Add Goal</Button>
         </form>
